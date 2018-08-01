9f7c871b24010c158f81f47a1b2c476820053ab9
belikovanastasya <belikova.ava@gmail.com>
2018-08-01 07:10:02 +0300

    get and post methods create

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
-const a = (a) => a
-console.log(a(5));
+const express = require('express');
+const bodyParser= require('body-parser')
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://<nastasya>:<artur30677>@ds161751.mlab.com:61751/attach', (err, database) => {
+  // ... start the server
+  app.use(bodyParser.urlencoded({extended: true}))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+app.post('/quotes', (req, res) => {
+})
+
+if (err) return console.log(err)
+  db = client.db('attach') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+  app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+  
+      console.log('saved to database')
+      res.redirect('/')
+    })
+  })
+
+})
 
